64b5d4a6d9d96e1de441e12364afbd6ff371b9d6
Ishan Banerjee <ishanbb@hotmail.com>
2021-01-06 21:21:19 -0600

    Implemented the code for erasing the sketch area

--- chalkdraw.js
+++ chalkdraw.js
@@ -19,6 +19,8 @@ window.onload = function () {
 
     //A Tool of object of the class paper.js, used for drawing 
     var markerTool = new Tool();
+    markerTool.minDistance = 5;
+    markerTool.activate();
 
     //Drawing section code
     markerTool.onMouseDown = function (event) {
@@ -37,4 +39,102 @@ window.onload = function () {
     markerTool.onMouseUp = function (event) {
       drawPath.simplify(modifiers.correctionVal);
     };
+
+    var eraserTool = new Tool();
+    eraserTool.minDistance = 10;
+    var eraserPath, tmpGroup, mask;
+
+    //Erasing section code
+    eraserTool.onMouseDown = function(event){
+        eraserPath = new Path({
+            strokeWidth: modifiers.eraseWidth * view.pixelRatio,
+            strokeCap: "round",    
+            strokeJoin: "round",
+            strokeColor: "black"
+        });
+
+        tmpGroup = new Group({
+            children: topLayer.removeChildren(),
+            blendMode: "source-out",
+            insert: false
+        });
+
+        mask = new Group({
+            children: [eraserPath, tmpGroup],
+            blendMode: "source-over",
+        });
+    };
+
+    eraserTool.onMouseDrag = function (event){
+        eraserPath.add(event.point);
+    };
+
+    eraserTool.onMouseUp = function (event){
+        var eraserRadius = (modifiers.eraseWidth * view.pixelRatio) / 2;
+
+        var outerPath = OffsetUtils.offsetPath(eraserPath, eraserRadius);
+        var innerPath = OffsetUtils.offsetPath(eraserPath, -eraserRadius);
+        eraserPath.remove();
+
+        outerPath.insert = false;
+        innerPath.insert = false;
+        innerPath.reverse();
+
+        var deleteShape = new Path({
+            closed: true,
+            insert: false,
+        });
+        deleteShape.addSegments(outerPath.segments);
+        deleteShape.addSegments(innerPath.segments);
+
+        var endCaps = new CompoundPath({
+            children: [
+              new Path.Circle({
+                center: eraserPath.firstSegment.point,
+                radius: eraserRadius,
+              }),
+              new Path.Circle({
+                center: eraserPath.lastSegment.point,
+                radius: eraserRadius,
+              }),
+            ],
+            insert: false,
+          });
+
+          deleteShape = deleteShape.unite(endCaps);
+          deleteShape.simplify();
+
+          var items = tmpGroup.getItems({overlapping: deleteShape.bounds});
+
+          items.forEach(function (item) {
+            var result = item.subtract(deleteShape, {
+              trace: false,
+              insert: false,
+            }); 
+    
+            if (result.children) {
+              
+              item.parent.insertChildren(item.index, result.removeChildren());
+              item.remove();
+            } else {
+              if (result.length === 0) {
+                item.remove();
+              } else {
+                item.replaceWith(result);
+              }
+            }
+          });
+    
+          topLayer.addChildren(tmpGroup.removeChildren());
+          mask.remove();
+          
+    };
+    $(document).keypress(function (event){
+        if(event.key == "e") 
+            eraserTool.activate();
+        else if(event.key == "d") {
+            markerTool.activate();
+        }
+    });
+
   };
