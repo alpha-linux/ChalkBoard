fc85e0799300d3b480bfadae2d334f45afc8189f
Ishan Banerjee <ishanbb@hotmail.com>
2021-01-07 23:49:27 -0600

    Started the implementation of the command line interface. Also changed the key that triggers the toolbar. This key binding will change in later versions

--- screenanimation.js
+++ screenanimation.js
@@ -1,12 +1,36 @@
+var clickrecord = false;
+
 $(document).keydown(function(event) {
-    if (event.which === 32) {
-        openToolBar()
+    if (event.key == "`" && clickrecord == false) {
+        openToolBar();
+        clickrecord = true;
+    }else{
+      closeToolBar();
+      clickrecord = false;
     }
   });
   
-  
+  $(document).keydown(function(event){
+    if(event.key == "/"){
+      openCommandLine();
+    }
+    if(event.key == "Escape")
+      closeCommandLine();
+  });
   
 function openToolBar(){
-    console.log("SpaceBar Triggred")
-      document.getElementById('toolbar').style.width = "2.2em";
+  document.getElementById('toolbar').style.width = "2.4em";
+}
+
+function openCommandLine(){
+  document.getElementById('footer').style.height = "40px";
+  document.getElementById('cli').focus()
+}
+
+function closeToolBar(){
+  document.getElementById('toolbar').style.width = "0em";
+}
+
+function closeCommandLine(){
+  document.getElementById('footer').style.height = "0";
 }
